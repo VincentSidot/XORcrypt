9a75c17f39fa7121b8e322ddb5ca22f95f440077
Vincent <vs38190@gmail.com>
2017-10-05 22:27:06 +0200

    Update XORcrypt.py

--- XORcrypt.py
+++ XORcrypt.py
@@ -1,4 +1,6 @@
+import utils
 alphabet = [chr(ord('A') + i) for i in range(0,26)] + [chr(ord('a') + i) for i in range(0,26)] + [chr(ord('0') + i) for i in range(0,10)]
+
 
 def XORcrypt(str,key):
     rep = []
@@ -48,4 +50,53 @@ def PMSDecrypt(str,key):
         except:
             rep += [' ']
     return ''.join(rep)
-        
+
+
+def CongruentialCrytp(str,key):
+    import hashlib
+    Xn = utils.HashlibToInt(hashlib.sha256(key.encode()))
+    L = []
+    for i in str:
+        try:
+            L += chr(ord(alphabet[(alphabet.index(i) + Xn)%len(alphabet)]))
+            Xn = utils.nextCongruential(Xn)
+        except:
+            L += [' ']
+    return ''.join(L)
+
+def CongruentialDerypt(str,key):
+    import hashlib
+    Xn = utils.HashlibToInt(hashlib.sha256(key.encode()))
+    L = []
+    for i in str:
+        try:
+            L += chr(ord(alphabet[(alphabet.index(i) - Xn)%len(alphabet)]))
+            Xn = utils.nextCongruential(Xn)
+        except:
+            L += [' ']
+    return ''.join(L)
+    
+def CongruentialPMSCrytp(str,key):
+    import hashlib
+    Xn = utils.HashlibToInt(hashlib.sha256(key.encode()))
+    L = []
+    for i in range(0,len(str)):
+        try:
+            L += chr(ord(alphabet[(alphabet.index(str[i]) + i*Xn)%len(alphabet)]))
+            Xn = utils.nextCongruential(Xn)
+        except:
+            L += [' ']
+    return ''.join(L)
+
+def CongruentialPMSDerypt(str,key):
+    import hashlib
+    Xn = utils.HashlibToInt(hashlib.sha256(key.encode()))
+    L = []
+    for i in range(0,len(str)):
+        try:
+            L += chr(ord(alphabet[(alphabet.index(str[i]) - i*Xn)%len(alphabet)]))
+            Xn = utils.nextCongruential(Xn)
+        except:
+            L += [' ']
+    return ''.join(L)
+    
